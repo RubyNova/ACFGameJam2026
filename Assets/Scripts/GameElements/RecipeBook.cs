e5190cc652b05136cbecb0901e2fc969719ab13b
Ken Johnson <k.rjohnson94@outlook.com>
2026-03-20 11:00:30 -0400

    Add test assets

---
+++ Assets/Scripts/GameElements/RecipeBook.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using CraftingAPI;
+using UnityEngine;
+
+namespace GameElements
+{
+    public class RecipeBook : MonoBehaviour
+    {
+
+
+        private void Start()
+        {
+            ItemDatabase.Instance.ItemDiscovered += DiscoveryEvent;
+        }
+
+        private void DiscoveryEvent(ItemConfig data)
+        {
+            if(data != null)
+                Debug.Log("We found a new recipe: "+ data.ItemName);
+        }
+
+    }
+}
