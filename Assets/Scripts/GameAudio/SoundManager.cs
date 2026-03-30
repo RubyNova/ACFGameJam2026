4f3cf01b9310fe4f155bf2d5e0ef5b22bd12667b
Matt Jones <matthewj866@gmail.com>
2026-03-30 22:16:14 +0100

    Code cleanup.

---
+++ Assets/Scripts/GameAudio/SoundManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using Utilities;
+
+namespace GameAudio
+{
+    public class SoundManager : MonoSingleton<SoundManager>
+    {
+        [SerializeField]
+        private float _bgmVolume = 0.5f;
+        [SerializeField]
+        private float _sfxVolume = 1;
+
+        private AudioSource _audioSource;
+
+        [SerializeField]
+        private AudioClip _bgm;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_audioSource != null && _bgm != null)
+            {
+                PlayBGM();
+            }
+        }
+
+        public void PlayAudioClip(AudioClip audioClip)
+        {
+            _audioSource.PlayOneShot(audioClip, _sfxVolume);
+        }
+
+        private void PlayBGM()
+        {
+            if (!_audioSource.isPlaying.Equals(_bgm))
+            {
+                _audioSource?.PlayOneShot(_bgm, _bgmVolume);
+            }
+        }
+
+        protected override void OnInit()
+        {
+
+        }
+
+    }
+}
--- Assets/Scripts/GameElements/CraftingPot.cs
+++ Assets/Scripts/GameElements/CraftingPot.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using CraftingAPI;
 using UnityEngine;
+using GameAudio;
 
 namespace GameElements
 {
