ba4ac23f55231442db553a92a065e82264444cea
Ken Johnson <k.rjohnson94@outlook.com>
2026-04-06 21:36:50 -0400

    Add test narrative

---
+++ Assets/Scripts/Player/ProgressController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class ProgressController : MonoBehaviour
+    {
+        private int _itemsDiscovered;
+        private int _itemsCrafted;
+        private int _itemsDelivered;
+        private int _itemsDeliveredSuccessfully;
+        private int _timeRemainingInSeconds;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        
+    }
+}
