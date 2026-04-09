74adc22dea2086a22c0b93ef745415c43b0edf44
Luke Culver <culver.luke@gmail.com>
2026-04-09 21:07:52 +0100

    Options menu now saves sound prefrences

--- Assets/Scripts/Menus/OptionsMenu.cs
+++ Assets/Scripts/Menus/OptionsMenu.cs
@@ -1,32 +1,54 @@
 using GameAudio;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
     private SoundManagerConfig _config;
 
-
-    public void ChangeMasterVolume(float volume)
-    {
-        _config.AudioMixer.SetFloat("MasterVolume", volume);
-    }
-
-    public void ChangeBgmVolume(float volume)
-    {
-        _config.AudioMixer.SetFloat("BgmVolume", volume);
-    }
-
-    public void ChangeSfxVolume(float volume)
-    {
-        _config.AudioMixer.SetFloat("SfxVolume", volume);
-    }
-
+    [SerializeField] private Slider _masterVolumeSlider;
+    [SerializeField] private Slider _bgmVolumeSlider;
+    [SerializeField] private Slider _sfxVolumeSlider;
+    [SerializeField] private GameObject _optionsUI;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _config = Resources.Load<SoundManagerConfig>("GameAudioConfigData/DefaultSoundConfig");
+        LoadVolumeData();
+        _optionsUI.SetActive(false);  //  PlayerPrefs not loading in if the options menu was disabled on game start
     }
+
+    public void ChangeMasterVolume(float volume)
+    {
+        _config.AudioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("MasterSavedVolume", volume);
+    }
+
+    public void ChangeBgmVolume(float volume)
+    {
+        _config.AudioMixer.SetFloat("BgmVolume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("BgmSavedVolume", volume);
+    }
+
+    public void ChangeSfxVolume(float volume)
+    {
+        _config.AudioMixer.SetFloat("SfxVolume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SfxSavedVolume", volume);
+    }
+
+    private void LoadVolumeData()
+    {
+        ChangeMasterVolume(PlayerPrefs.GetFloat("MasterSavedVolume"));
+        _masterVolumeSlider.value = PlayerPrefs.GetFloat("MasterSavedVolume");
+
+        ChangeBgmVolume(PlayerPrefs.GetFloat("BgmSavedVolume"));
+        _bgmVolumeSlider.value = PlayerPrefs.GetFloat("BgmSavedVolume");
+
+        ChangeSfxVolume(PlayerPrefs.GetFloat("SfxSavedVolume"));
+        _sfxVolumeSlider.value = PlayerPrefs.GetFloat("SfxSavedVolume");
+    }
+
 
     // Update is called once per frame
     void Update()
