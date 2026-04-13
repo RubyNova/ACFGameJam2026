9e1e3a6517d9fb5913df5e527e8383e00b0c8fa2
Matt Jones <matthewj866@gmail.com>
2026-04-13 23:32:00 +0100

    Update previous feature.

--- Assets/Scripts/GameElements/ItemInstance.cs
+++ Assets/Scripts/GameElements/ItemInstance.cs
@@ -12,35 +12,24 @@ namespace GameElements
 
         private Texture2D _clearTexture;
 
-        private bool _hasStarted = false;
+        private bool _hasFirstTimeInitialised = false;
 
         public ItemConfig BackingConfig { get; private set; }
 
-        private void Start()
-        {
-            if (_hasStarted)
-            {
-                return;
-            }
-
-            _hasStarted = true;
-
-            _runtimeMaterial = Instantiate(_itemRenderer.material);
-            _itemRenderer.material = _runtimeMaterial;
-
-            _clearTexture = new Texture2D(1, 1);
-            _clearTexture.SetPixel(0, 0, Color.clear);
-            _clearTexture.Apply();
-        }
-
-        private void OnDestroy()
-        {
-            Destroy(_runtimeMaterial);
-        }
-
         public void InitialiseWithItemConfig(ItemConfig config)
         {
-            Start();
+            if (!_hasFirstTimeInitialised)
+            {
+                _hasFirstTimeInitialised = true;
+
+                _runtimeMaterial = Instantiate(_itemRenderer.material);
+                _itemRenderer.material = _runtimeMaterial;
+
+                _clearTexture = new Texture2D(1, 1);
+                _clearTexture.SetPixel(0, 0, Color.clear);
+                _clearTexture.Apply();
+            }
+
             BackingConfig = config;
             _itemRenderer.sprite = BackingConfig.ItemIconBackground;
 
