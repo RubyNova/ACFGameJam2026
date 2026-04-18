d6a819e236ca4e7542a55bfff98754bf3a651a8c
Matt Jones <matthewj866@gmail.com>
2026-04-18 16:29:32 +0100

    Speculative fix for recipe book breakage.

--- Assets/Scripts/GameElements/RecipeBook.cs
+++ Assets/Scripts/GameElements/RecipeBook.cs
@@ -12,10 +12,9 @@ namespace GameElements
         [SerializeField]
         private TMP_Text _discoveryTextBox;
 
-        private void Start()
-        {
-            ItemDatabase.Instance.ItemDiscovered += DiscoveryEvent;
-        }
+        private void Start() => ItemDatabase.Instance.ItemDiscovered += DiscoveryEvent;
+
+        private void OnDestroy() => ItemDatabase.Instance.ItemDiscovered -= DiscoveryEvent;
 
         private void DiscoveryEvent(ItemConfig data)
         {
