e9b567cdf8acdc8956cecfaea9ed670e8baa1b36
Luke Culver <culver.luke@gmail.com>
2026-04-19 05:00:32 +0100

    Small scene changes on NameStealingScene, added shrinking paper because the scene transition seemed sudden

--- Assets/Scripts/GameElements/NameStealer.cs
+++ Assets/Scripts/GameElements/NameStealer.cs
@@ -1,4 +1,5 @@
 using ACHNarrativeDriver.Api;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,12 +11,20 @@ public class NameStealer : MonoBehaviour
     [SerializeField]
     private RuntimeVariables _runtimeVariables;
 
+    [SerializeField]
+    private RectTransform _paperSprite;
+
+    private int _delay = 3;
+
+    private int _shrinkSpeed;
+
+    private float _scale = 1f;
 
     public void GetFromInputField(string input)
     {
         _inputText = input;
         _runtimeVariables.UpdateVariable("PlayerName", _inputText);
-        LoadDay1();
+        StartCoroutine(LoadingProcess());
     }
 
     private void LoadDay1()
@@ -23,6 +32,20 @@ public class NameStealer : MonoBehaviour
         LevelManager.Instance.LoadScene("Day1");
     }
 
+    private IEnumerator LoadingProcess()
+    {
+        yield return new WaitForSecondsRealtime(1);
+
+        while(_scale > 0.01f)
+        {
+            _scale -= 0.03f;
+            _paperSprite.localScale = new Vector3(_scale, _scale, 0);
+            yield return new WaitForSecondsRealtime(0.04f);
+        };
+
+        LoadDay1();
+        Object.Destroy(_paperSprite.gameObject);
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
